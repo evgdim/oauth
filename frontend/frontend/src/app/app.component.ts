6e86fa3288db19362e5bda233c348276b9982150
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2019-07-23 22:31:57 +0300

    angular social

--- frontend/frontend/src/app/app.component.ts
+++ frontend/frontend/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/services/app.service';
 
+import { AuthService } from "angularx-social-login";
+import { FacebookLoginProvider, GoogleLoginProvider } from "angularx-social-login";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,11 +12,14 @@ import { AppService } from 'src/services/app.service';
 export class AppComponent implements OnInit{
   
 
-  constructor(private appService: AppService) {}
+  constructor(private authService: AuthService) {}
   title = 'frontend';
 
   ngOnInit(): void {
-    this.appService.obtainAccessToken()
-    this.title = this.appService.getAccessToken()
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+   
+    this.authService.authState.subscribe((user) => {
+      console.log(user);
+    });
   }
 }
--- frontend/frontend/src/app/app.module.ts
+++ frontend/frontend/src/app/app.module.ts
@@ -4,15 +4,35 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { SocialLoginModule, AuthServiceConfig } from "angularx-social-login";
+import { GoogleLoginProvider } from "angularx-social-login";
+
+let config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("113294925702-gqfvecvk5b4ndual6brpkc1hircvjhu5.apps.googleusercontent.com")
+  }
+]);
+
+export function provideConfig() {
+  return config;
+}
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    SocialLoginModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
